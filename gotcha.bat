91e4df100ffe12c86610dd0c1ade3169a565297b
Michael Cookson <michael.cookson@fullbay.com>
2026-01-12 22:16:19 -0700

    Add files via upload

---
+++ gotcha.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ================================
+echo    SECURITY TEST - GOTCHA!
+echo ================================
+echo.
+echo If you're seeing this, an executable
+echo made it through the attachment filter.
+echo.
+pause
